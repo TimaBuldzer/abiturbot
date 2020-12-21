751d53dca9569aca22ff2b72e5da6f40c656c551
Tim <timamasharipov47@gmail.com>
2020-12-21 20:28:50 +0500

    initial configuration

---
+++ conf/settings/local.py
@@ -0,0 +1,20 @@
+SECRET_KEY = '0-slckcp=*2r3z7gsemkmj8+f-is+huz$jy@2xtrrewn7kp--f'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'abitur',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = '/static/'
+
+from .base import *
